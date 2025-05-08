8351d18f79e96641fedff96d195ec34245f2786c
Singhamit911 <Singhamitbhanu@gmail.com>
2025-05-08 17:02:46 +0530

    updated git ignore

--- src/main/java/com/learningRESTAPI/journalApp/service/WeatherService.java
+++ src/main/java/com/learningRESTAPI/journalApp/service/WeatherService.java
@@ -2,14 +2,18 @@ package com.learningRESTAPI.journalApp.service;
 
 import com.learningRESTAPI.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
-    private static final String apikey = "6e465ced7f21c5207b02e1ebc68abaf0";
+
+    @Value("${weather.api.key}")
+    private String apikey;
 
     private static final String API = "https://api.openweathermap.org/data/2.5/weather?q=CITY&appid=API_KEY&units=metric";
     //https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=YOUR_API_KEY&units=imperial
